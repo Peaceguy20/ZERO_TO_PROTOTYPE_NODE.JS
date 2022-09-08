a0baebb9c40cb63a70beea80b3e99557186ceeac
Peaceguy20 <103639053+Peaceguy20@users.noreply.github.com>
2022-09-08 11:29:31 +0530

    Add files via upload

---
+++ http.js
@@ -0,0 +1,17 @@
+var http = require("http");
+var a=10;
+var b =20;
+var c=a+b;
+var msg="Sum is " + c;
+if(c==30)
+{
+msg += "<br/> C is 30";
+}
+else {
+msg += "C is not 30";
+}
+http.createServer(function(req,res){
+res.writeHead(200,{'Content-Type':'text/html'});
+res.end(msg);
+}).listen(3001);
+ console.log("server is listening on http://127.0.0.1:3001");
---
+++ welcome.js
@@ -0,0 +1,20 @@
+console.log("welcome to node js ");
+var a=10;
+var b=45;
+var c=a+b;
+var d=a*b;
+console.log("a value is " + a + " b value is " + b);  
+console.log("sum is " +c);
+console.log("mul is " +d);
+
+console.log(`A value is ${a} and B value is ${b}
+and sum is ${a+b} and mul is ${a*b}`);
+ if(c=30)
+{
+console.log("sum is 30");
+}
+else
+{
+console.log("else");
+}
+ 
